82087f55c2d364a4e5c5c41ff75862fba32a2767
MohamedGamal10 <50083096+MohamedGamal10@users.noreply.github.com>
2023-04-26 13:56:20 +0200

    dfgfdg

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Mohamed
+          Learn React Mohamed & Ahmed
         </a>
       </header>
     </div>
